d3622c016a4d72f9add8f9b8ed4508a04768151b
yunjipark0623 <bearji0623@naver.com>
2018-09-17 23:44:27 +0900

    180917

---
+++ 숫자형 연습/Main.js
@@ -0,0 +1,14 @@
+var width = 10;
+var height = 20;
+
+// 둘레의 길이 구하기
+var perimeter;
+perimeter = (2 * width) + (2 * height);
+
+// 넓이 구하기
+var area;
+area = width * height;
+
+// 테스트 코드
+console.log("둘레의 길이: " + perimeter);
+console.log("넓이: " + area);
